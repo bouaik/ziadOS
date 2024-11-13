3d7b3f32199c5cc22ac3d52147ee1d168455fca1
bouaik <bouaik.lhou@gmail.com>
2024-11-13 14:28:16 +0100

    draw canvas

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("hello world")
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.height = document.documentElement.clientHeight
+canvas.width = document.documentElement.clientWidth
+
+ctx.fillStyle = "green"
+ctx.fillRect(10, 10, 150, 100)
