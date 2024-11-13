feaa143c20e343dee3624431800d134bcc9aec42
bouaik <bouaik.lhou@gmail.com>
2024-11-13 14:29:33 +0100

    remove colors

--- main.js
+++ main.js
@@ -4,5 +4,4 @@ const ctx = canvas.getContext("2d")
 canvas.height = document.documentElement.clientHeight
 canvas.width = document.documentElement.clientWidth
 
-ctx.fillStyle = "green"
 ctx.fillRect(10, 10, 150, 100)
