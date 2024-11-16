02a9271045797b4045050e3a4dc9a687b3394b90
bouaik <bouaik.lhou@gmail.com>
2024-11-16 15:15:43 +0100

    align icons horizontally

--- src/Desktop.js
+++ src/Desktop.js
@@ -5,7 +5,7 @@ export default class Desktop {
 
   static start() {
     const icon1 = new Icon(10, 10, 50, 50, "Calculator")
-    const icon2 = new Icon(10, 90, 50, 50, "Editor")
+    const icon2 = new Icon(70, 10, 50, 50, "Editor")
     this.icons.push(icon1)
     this.icons.push(icon2)
   }
